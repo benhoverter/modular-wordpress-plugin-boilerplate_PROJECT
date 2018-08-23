5ef10850b9f2e3dcce36099e92160bfddec03ffa
Ben Hoverter <ben.hoverter@gmail.com>
2018-08-23 10:53:28 -0700

    Cache flush to fix folder names.

---
+++ plugin-name/admin/element/js/element.js
@@ -0,0 +1,13 @@
+(function( $ ) {
+	'use strict';
+
+    $(document).ready( function() {
+
+        const testfunc = ( a ) => a * a;
+
+        console.log( testfunc( 23 ) );
+
+
+    } ); // END: document.ready
+
+})( jQuery );
