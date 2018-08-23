e3f99d3f2469d44dc5e78710416f87001ad60c55
Ben Hoverter <ben.hoverter@gmail.com>
2018-08-23 15:14:58 -0700

    Fixed indentation.

--- plugin-name/admin/element/views/element-view.php
+++ plugin-name/admin/element/views/element-view.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Provide an admin-facing view for the plugin
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin/element/views
- */
+* Provide an admin-facing view for the plugin
+*
+* @link       http://example.com
+* @since      1.0.0
+*
+* @package    Plugin_Name
+* @subpackage Plugin_Name/admin/element/views
+*/
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. No classes! -->
--- plugin-name/public/element/views/element-view.php
+++ plugin-name/public/element/views/element-view.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Provide a public-facing view for the plugin
- *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/public/element/views
- */
+* Provide a public-facing view for the plugin
+*
+* @link       http://example.com
+* @since      1.0.0
+*
+* @package    Plugin_Name
+* @subpackage Plugin_Name/public/element/views
+*/
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. No classes! -->
