984c6a67c471127b1989162167e6613821e844fb
Ben Hoverter <ben.hoverter@gmail.com>
2018-08-27 14:19:32 -0700

    Removed set_elements() from Admin and Public.  Works.

--- plugin-name/admin/Admin.php
+++ plugin-name/admin/Admin.php
@@ -94,129 +94,112 @@ class Plugin_Abbr_Admin {
     * @param      string    $plugin_name       The name of the plugin.
     * @param      string    $version    The version of this plugin.
     */
-public function __construct( $plugin_name, $version /*, $conn, $queries */ ) {
+    public function __construct( $plugin_name, $version /*, $conn, $queries */ ) {
 
-    $this->plugin_name = $plugin_name;
-    $this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-    $this->load_dependencies( $plugin_name, $version );
-    $this->set_elements( $plugin_name, $version );
+        $this->load_dependencies( $plugin_name, $version );
 
-    // For DB interactions:
-    //$this->conn = $conn;
-    //$this->queries = $queries;
+        // For DB interactions:
+        //$this->conn = $conn;
+        //$this->queries = $queries;
 
-}
+    }
 
-
-/**
-* Load the required dependencies for the Admin class elements.
-*
-* Should require_once each Element class file in /admin/element.
-*
-* @since    1.0.0
-* @access   private
-*/
-private function load_dependencies( $plugin_name, $version ) {
 
     /**
-    * The element responsible for ________.
-    */
-    require_once plugin_dir_path( __FILE__ ) . 'element/Element.php';
-    $this->element = new Plugin_Abbr_Admin_Element( $plugin_name, $version );
-
-    /**
-    * The AJAX element responsible for ________.
-    */
-    require_once plugin_dir_path( __FILE__ ) . 'element-ajax/Element-Ajax.php';
-    $this->element_ajax = new Plugin_Abbr_Admin_Element_Ajax( $plugin_name, $version );
-
-}
-
-
-/**
-* Register the combined stylesheet for the admin-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_styles() {
-
-    /**
-    * An instance of this class is passed to the run() function
-    * defined in Plugin_Abbr_Loader, which then creates the relationship
-    * between the defined hooks and the functions defined in this
-    * class.
+    * Load the required dependencies for the Admin class elements.
     *
-    * This architecture assumes you are transpiling all child directory
-    * css/scss/less files into a single admin.css file in the /admin directory.
+    * Should require_once each Element class file in /admin/element.
+    *
+    * @since    1.0.0
+    * @access   private
     */
+    private function load_dependencies( $plugin_name, $version ) {
 
-    // Variable to hold the URL path for enqueueing.
-    $admin_css_dir_url = plugin_dir_url( __DIR__ ) . 'assets/admin/admin.min.css';
+        /**
+        * The element responsible for ________.
+        */
+        require_once plugin_dir_path( __FILE__ ) . 'element/Element.php';
+        $this->element = new Plugin_Abbr_Admin_Element( $plugin_name, $version );
 
-    // Variable to hold the server path for filemtime() and versioning.
-    $admin_css_dir_path = plugin_dir_path( __DIR__ ) . 'assets/admin/admin.min.css';
+        /**
+        * The AJAX element responsible for ________.
+        */
+        require_once plugin_dir_path( __FILE__ ) . 'element-ajax/Element-Ajax.php';
+        $this->element_ajax = new Plugin_Abbr_Admin_Element_Ajax( $plugin_name, $version );
 
-    // Register the style using an automatic and unique version based on modification time.
-    wp_register_style( $this->plugin_name, $admin_css_dir_url, array(), filemtime( $admin_css_dir_path ), 'all' );
+    }
 
-    // Enqueue the style.
-    wp_enqueue_style( $this->plugin_name );
-    //wp_enqueue_style( 'thickbox' );
-
-}
-
-/**
-* Register the concat/minified JavaScript for the admin-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_scripts() {
 
     /**
-    * An instance of this class is passed to the run() function
-    * defined in Plugin_Abbr_Loader, which then creates the relationship
-    * between the defined hooks and the functions defined in this
-    * class.
+    * Register the combined stylesheet for the admin-facing side of the site.
     *
-    * This architecture assumes you are transpiling all child directory
-    * JavaScript files into "/admin/admin.min.js".
+    * @since    1.0.0
     */
+    public function enqueue_styles() {
 
-    // Variable to hold the URL path for enqueueing.
-    $admin_js_dir_url = plugin_dir_url( __DIR__ ) . 'assets/admin/admin.min.js';
+        /**
+        * An instance of this class is passed to the run() function
+        * defined in Plugin_Abbr_Loader, which then creates the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        *
+        * This architecture assumes you are transpiling all child directory
+        * css/scss/less files into a single admin.css file in the /admin directory.
+        */
 
-    // Variable to hold the server path for filemtime() and versioning.
-    $admin_js_dir_path = plugin_dir_path( __DIR__ ) . 'assets/admin/admin.min.js';
+        // Variable to hold the URL path for enqueueing.
+        $admin_css_dir_url = plugin_dir_url( __DIR__ ) . 'assets/admin/admin.min.css';
 
-    // Register the script using an automatic and unique version based on modification time.
-    wp_register_script( $this->plugin_name, $admin_js_dir_url, array( 'jquery' ), filemtime( $admin_js_dir_path ), true );
+        // Variable to hold the server path for filemtime() and versioning.
+        $admin_css_dir_path = plugin_dir_path( __DIR__ ) . 'assets/admin/admin.min.css';
 
-    // Enqueue the scripts.
-    wp_enqueue_script( $this->plugin_name );
+        // Register the style using an automatic and unique version based on modification time.
+        wp_register_style( $this->plugin_name, $admin_css_dir_url, array(), filemtime( $admin_css_dir_path ), 'all' );
 
-    // PHP data for the frontend.  Do one wp_localize_script() call per element.
-    // Localize the script to make PHP data available to AJAX JS.  Define data in Element-Ajax.php.
-    wp_localize_script( $this->plugin_name, 'abbr_admin_element_data', $this->element_ajax->get_ajax_data() );
+        // Enqueue the style.
+        wp_enqueue_style( $this->plugin_name );
+        //wp_enqueue_style( 'thickbox' );
 
-}
+    }
 
+    /**
+    * Register the concat/minified JavaScript for the admin-facing side of the site.
+    *
+    * @since    1.0.0
+    */
+    public function enqueue_scripts() {
 
-/**
-* Sets instances of each admin element class.
-* Those elements' methods are hooked in /includes/Main.php.
-*
-* Initialize each element object in the property list above,
-* then instance them here!
-*
-* @since    1.0.0
-*/
-public function set_elements( $plugin_name, $version ) {
+        /**
+        * An instance of this class is passed to the run() function
+        * defined in Plugin_Abbr_Loader, which then creates the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        *
+        * This architecture assumes you are transpiling all child directory
+        * JavaScript files into "/admin/admin.min.js".
+        */
 
-    $this->element = new Plugin_Abbr_Admin_Element( $plugin_name, $version );
-    $this->element_ajax = new Plugin_Abbr_Admin_Element_Ajax( $plugin_name, $version );
+        // Variable to hold the URL path for enqueueing.
+        $admin_js_dir_url = plugin_dir_url( __DIR__ ) . 'assets/admin/admin.min.js';
 
-}
+        // Variable to hold the server path for filemtime() and versioning.
+        $admin_js_dir_path = plugin_dir_path( __DIR__ ) . 'assets/admin/admin.min.js';
+
+        // Register the script using an automatic and unique version based on modification time.
+        wp_register_script( $this->plugin_name, $admin_js_dir_url, array( 'jquery' ), filemtime( $admin_js_dir_path ), true );
+
+        // Enqueue the scripts.
+        wp_enqueue_script( $this->plugin_name );
+
+        // PHP data for the frontend.  Do one wp_localize_script() call per element.
+        // Localize the script to make PHP data available to AJAX JS.  Define data in Element-Ajax.php.
+        wp_localize_script( $this->plugin_name, 'abbr_admin_element_data', $this->element_ajax->get_ajax_data() );
+
+    }
+
 
 
 }
--- plugin-name/public/Public.php
+++ plugin-name/public/Public.php
@@ -85,129 +85,112 @@ class Plugin_Abbr_Public {
     * @param      string    $plugin_name       The name of the plugin.
     * @param      string    $version    The version of this plugin.
     */
-public function __construct( $plugin_name, $version /*, $conn, $queries */ ) {
+    public function __construct( $plugin_name, $version /*, $conn, $queries */ ) {
 
-    $this->plugin_name = $plugin_name;
-    $this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-    $this->load_dependencies( $plugin_name, $version );
-    $this->set_elements( $plugin_name, $version );
+        $this->load_dependencies( $plugin_name, $version );
 
-    // For DB interactions:
-    //$this->conn = $conn;
-    //$this->queries = $queries;
+        // For DB interactions:
+        //$this->conn = $conn;
+        //$this->queries = $queries;
 
-}
+    }
 
-
-/**
-* Load the required dependencies for the Public class' elements.
-*
-* Should require_once each Element class file in /public/element.
-*
-* @since    1.0.0
-* @access   private
-*/
-private function load_dependencies( $plugin_name, $version ) {
 
     /**
-    * The element responsible for ________.
-    */
-    require_once plugin_dir_path( __FILE__ ) . 'element/Element.php';
-    $this->element = new Plugin_Abbr_Public_Element( $plugin_name, $version );
-
-    /**
-    * The AJAX element responsible for ________.
-    */
-    require_once plugin_dir_path( __FILE__ ) . 'element-ajax/Element-Ajax.php';
-    $this->element_ajax = new Plugin_Abbr_Public_Element_Ajax( $plugin_name, $version );
-
-}
-
-
-/**
-* Register the combined stylesheet for the public-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_styles() {
-
-    /**
-    * An instance of this class is passed to the run() function
-    * defined in Plugin_Abbr_Loader, which then creates the relationship
-    * between the defined hooks and the functions defined in this
-    * class.
+    * Load the required dependencies for the Public class' elements.
     *
-    * This architecture assumes you are transpiling all child directory
-    * css/scss/less files into a single public.css file in the /public directory.
+    * Should require_once each Element class file in /public/element.
+    *
+    * @since    1.0.0
+    * @access   private
     */
+    private function load_dependencies( $plugin_name, $version ) {
 
-    // Variable to hold the URL path for enqueueing.
-    $public_css_dir_url = plugin_dir_url( __DIR__ ) . 'assets/public/public.min.css';
+        /**
+        * The element responsible for ________.
+        */
+        require_once plugin_dir_path( __FILE__ ) . 'element/Element.php';
+        $this->element = new Plugin_Abbr_Public_Element( $plugin_name, $version );
 
-    // Variable to hold the server path for filemtime() and versioning.
-    $public_css_dir_path = plugin_dir_path( __DIR__ ) . 'assets/public/public.min.css';
+        /**
+        * The AJAX element responsible for ________.
+        */
+        require_once plugin_dir_path( __FILE__ ) . 'element-ajax/Element-Ajax.php';
+        $this->element_ajax = new Plugin_Abbr_Public_Element_Ajax( $plugin_name, $version );
 
-    // Register the style using an automatic and unique version based on modification time.
-    wp_register_style( $this->plugin_name, $public_css_dir_url, array(),  filemtime( $public_css_dir_path ), 'all' );
+    }
 
-    // Enqueue the style.
-    wp_enqueue_style( $this->plugin_name );
-    //wp_enqueue_style( 'thickbox' );
-
-}
-
-/**
-* Register the concat/minified JavaScript for the public-facing side of the site.
-*
-* @since    1.0.0
-*/
-public function enqueue_scripts() {
 
     /**
-    * An instance of this class is passed to the run() function
-    * defined in Plugin_Abbr_Loader, which then creates the relationship
-    * between the defined hooks and the functions defined in this
-    * class.
+    * Register the combined stylesheet for the public-facing side of the site.
     *
-    * This architecture assumes that you are transpiling all child directory
-    * JavaScript files into a single public.min.js file in the /public directory.
+    * @since    1.0.0
     */
+    public function enqueue_styles() {
 
-    // Variable to hold the URL path for enqueueing.
-    $public_js_dir_url = plugin_dir_url( __DIR__ ) . 'assets/public/public.min.js';
+        /**
+        * An instance of this class is passed to the run() function
+        * defined in Plugin_Abbr_Loader, which then creates the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        *
+        * This architecture assumes you are transpiling all child directory
+        * css/scss/less files into a single public.css file in the /public directory.
+        */
 
-    // Variable to hold the server path for filemtime() and versioning.
-    $public_js_dir_path = plugin_dir_path( __DIR__ ) . 'assets/public/public.min.js';
+        // Variable to hold the URL path for enqueueing.
+        $public_css_dir_url = plugin_dir_url( __DIR__ ) . 'assets/public/public.min.css';
 
-    // Register the script using an automatic and unique version based on modification time.
-    wp_register_script( $this->plugin_name, $public_js_dir_url, array( 'jquery' ),  filemtime( $public_js_dir_path ), true );
+        // Variable to hold the server path for filemtime() and versioning.
+        $public_css_dir_path = plugin_dir_path( __DIR__ ) . 'assets/public/public.min.css';
 
-    // Enqueue the scripts.
-    wp_enqueue_script( $this->plugin_name );
+        // Register the style using an automatic and unique version based on modification time.
+        wp_register_style( $this->plugin_name, $public_css_dir_url, array(),  filemtime( $public_css_dir_path ), 'all' );
 
-    // PHP data for the frontend.  One wp_localize_script() call per element.
-    // Localize the script to make PHP data available to AJAX JS.  Define data in Element-Ajax.php.
-    wp_localize_script( $this->plugin_name, 'abbr_public_element_data', $this->element_ajax->get_ajax_data() );
+        // Enqueue the style.
+        wp_enqueue_style( $this->plugin_name );
+        //wp_enqueue_style( 'thickbox' );
 
-}
+    }
 
+    /**
+    * Register the concat/minified JavaScript for the public-facing side of the site.
+    *
+    * @since    1.0.0
+    */
+    public function enqueue_scripts() {
 
-/**
-* Sets instances of each public element class.
-* Those elements' methods are hooked in /includes/Main.php.
-*
-* Initialize each element object in the property list above,
-* then instance them here!
-*
-* @since    1.0.0
-*/
-public function set_elements( $plugin_name, $version ) {
+        /**
+        * An instance of this class is passed to the run() function
+        * defined in Plugin_Abbr_Loader, which then creates the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        *
+        * This architecture assumes that you are transpiling all child directory
+        * JavaScript files into a single public.min.js file in the /public directory.
+        */
 
-    $this->element = new Plugin_Abbr_Public_Element( $plugin_name, $version );
-    $this->element_ajax = new Plugin_Abbr_Public_Element_Ajax( $plugin_name, $version );
+        // Variable to hold the URL path for enqueueing.
+        $public_js_dir_url = plugin_dir_url( __DIR__ ) . 'assets/public/public.min.js';
 
-}
+        // Variable to hold the server path for filemtime() and versioning.
+        $public_js_dir_path = plugin_dir_path( __DIR__ ) . 'assets/public/public.min.js';
+
+        // Register the script using an automatic and unique version based on modification time.
+        wp_register_script( $this->plugin_name, $public_js_dir_url, array( 'jquery' ),  filemtime( $public_js_dir_path ), true );
+
+        // Enqueue the scripts.
+        wp_enqueue_script( $this->plugin_name );
+
+        // PHP data for the frontend.  One wp_localize_script() call per element.
+        // Localize the script to make PHP data available to AJAX JS.  Define data in Element-Ajax.php.
+        wp_localize_script( $this->plugin_name, 'abbr_public_element_data', $this->element_ajax->get_ajax_data() );
+
+    }
+
 
 
 }
